5da62886ef020ad015ff9e60b157b2d8535d09aa
Emanuelvtsg <evtsg@cin.ufpe.br>
2022-05-10 21:00:02 -0300

    modificacao no modo de cancelar, pagina mais recursiva

--- app/src/app/cancelar-voo/cancelar-voo.component.ts
+++ app/src/app/cancelar-voo/cancelar-voo.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Voo } from './voo'
-// import { Senha } from 'localOndeFicaraSenha';
+
+import { ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-cancelar-voo',
@@ -12,22 +13,52 @@ import { Voo } from './voo'
 export class CancelarVooComponent implements OnInit {
   voo1 = {codigo: '130123', embarque: '13/06/2022', origem: 'Recife, Congonhas', destino: 'São Paulo, Guarulhos'};
   voo2 = {codigo: '225323', embarque: '18/08/2022', origem: 'São Paulo, Guarulhos', destino: 'Recife, Congonhas'};
-  voos: Voo[] = [this.voo1, this.voo2];
+  voo3 = {codigo: '335723', embarque: '13/05/2023', origem: 'Manaus, Aeroporto de Manaus', destino: 'Rio de Janeiro, Guararapes'}
+  voos: Voo[] = [this.voo1, this.voo2, this.voo3];
   codigoCancelar: string;
+  digitouErrado: boolean = false;
+  campoConfirmacao: string = "";
+  @ViewChild('fecharPopUp') fecharPopUp: any;
   
   constructor() { 
     this.codigoCancelar = ""
   }
 
+  limparCampoConfirmacao(){
+    this.campoConfirmacao = "";
+  }
+
   cancelarVoo(codigo: string): void {
     var result: Voo[] = [];
-    for (let v of this.voos) {
-      if (codigo != v.codigo){
-        result.push(v);
+    var codigoCancelado: string = "";
+    if (this.digitouCorreto()){
+      for (let v of this.voos) {
+        if (codigo != v.codigo){
+          result.push(v);
+        }
+        else {
+          codigoCancelado = v.codigo;
+        }
       }
+      this.voos = result;
+      this.codigoCancelar = "";
+      this.limparCampoConfirmacao();
+      this.fecharPopUp.nativeElement.click();
+      alert("Você cancelou o vôo " + codigoCancelado + " com sucesso!")
     }
-    this.voos = result;
-    this.codigoCancelar = "";
+    else {
+      this.digitouErrado = true;
+      this.limparCampoConfirmacao();
+    }
+  }
+
+  digitouCorreto(): boolean {
+    if (this.campoConfirmacao == "QUEROCANCELAR") {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   armazenaCodigo(codigo: string): void {
@@ -40,12 +71,12 @@ export class CancelarVooComponent implements OnInit {
     codigo = this.codigoCancelar;
     return codigo;
   }
+  
+  onMove(): void {
+    this.digitouErrado = false;
+  }
 
   ngOnInit(): void {
   }
-
-  // senhaParaCancelar(senha: Senha): void {
-
-  //}
 
 }
